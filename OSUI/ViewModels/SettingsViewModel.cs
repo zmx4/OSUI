04cef251a5373ac6b5a737722501c4b4d8c8e988
Tick Zhong <1601858426@qq.com>
2026-05-15 10:10:42 +0800

    about

--- OSUI/ViewModels/SettingsViewModel.cs
+++ OSUI/ViewModels/SettingsViewModel.cs
@@ -2,18 +2,23 @@
 using System.Windows;
 using System.Windows.Controls;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using OSUI.Services;
 
 namespace OSUI.ViewModels;
 
 public partial class SettingsViewModel : PageViewModel
 {
+    private readonly IAuthService _authService;
+    
     [ObservableProperty]
     private ObservableCollection<string>? _themeNames;
     
-    public SettingsViewModel()
+    public SettingsViewModel(IAuthService  authService)
     {
+        _authService = authService;
         var app = Application.Current;
-        ThemeNames = app.Resources["ThemeNames"] as ObservableCollection<string>;
+        ThemeNames = ["Default"];
     }
 
     #region 主题切换
@@ -37,5 +42,18 @@ public partial class SettingsViewModel : PageViewModel
     }
 
     #endregion
-    
+
+    #region 关于窗口
+
+    [RelayCommand]
+    private void ShowAboutWindow()
+    {
+        var userName = _authService.CurrentUser?.Username;
+        MessageBox.Show($"OSUI - \n" +
+                        $"当前用户: {userName ?? "未登录"}" +
+                        $"\n版本: 1.0.0\n© 2024 OSUI Team", 
+                    "关于 OSUI");
+    }
+
+    #endregion
 }
