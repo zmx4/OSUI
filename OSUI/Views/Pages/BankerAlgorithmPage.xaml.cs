0e9d96fbe23e32aab68757901e38582ce6467724
Tick Zhong <1601858426@qq.com>
2026-05-26 09:09:55 +0800

    优化银行家算法页面布局，合并资源列并调整数据网格

--- OSUI/Views/Pages/BankerAlgorithmPage.xaml.cs
+++ OSUI/Views/Pages/BankerAlgorithmPage.xaml.cs
@@ -73,37 +73,27 @@ public partial class BankerAlgorithmPage : UserControl
 
     private void BuildResourceColumns(int resourceCount)
     {
-        AllocationGrid.Columns.Clear();
-        MaxGrid.Columns.Clear();
-        NeedGrid.Columns.Clear();
+        ProcessGrid.Columns.Clear();
         AvailableGrid.Columns.Clear();
 
         var processHeader = LocalizationService.Instance.GetString("Banker.Column.ProcessId");
-        AllocationGrid.Columns.Add(new DataGridTextColumn
+        ProcessGrid.Columns.Add(new DataGridTextColumn
         {
             Header = processHeader,
             Binding = new Binding("Id"),
             IsReadOnly = true
         });
-        MaxGrid.Columns.Add(new DataGridTextColumn
-        {
-            Header = processHeader,
-            Binding = new Binding("Id"),
-            IsReadOnly = true
-        });
-        NeedGrid.Columns.Add(new DataGridTextColumn
-        {
-            Header = processHeader,
-            Binding = new Binding("Id"),
-            IsReadOnly = true
-        });
+
+        var allocationLabel = LocalizationService.Instance.GetString("Banker.Table.Allocation");
+        var maxLabel = LocalizationService.Instance.GetString("Banker.Table.Max");
+        var needLabel = LocalizationService.Instance.GetString("Banker.Table.Need");
 
         for (var i = 0; i < resourceCount; i++)
         {
             var header = LocalizationService.Instance.Format("Banker.Column.Resource", i + 1);
-            AllocationGrid.Columns.Add(CreateResourceColumn(header, $"Allocation[{i}]", false));
-            MaxGrid.Columns.Add(CreateResourceColumn(header, $"Max[{i}]", false));
-            NeedGrid.Columns.Add(CreateResourceColumn(header, $"Need[{i}]", true));
+            ProcessGrid.Columns.Add(CreateResourceColumn($"{allocationLabel}-{header}", $"Allocation[{i}]", false));
+            ProcessGrid.Columns.Add(CreateResourceColumn($"{maxLabel}-{header}", $"Max[{i}]", false));
+            ProcessGrid.Columns.Add(CreateResourceColumn($"{needLabel}-{header}", $"Need[{i}]", true));
             AvailableGrid.Columns.Add(CreateResourceColumn(header, $"[{i}]", false));
         }
     }
